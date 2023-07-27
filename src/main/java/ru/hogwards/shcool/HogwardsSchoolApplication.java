06245f3c5a95b911b77f9d6c55053f409399519c
shaft163 <shaft163rus@gmail.com>
2023-07-27 04:07:47 +0400

    Initial commit

---
+++ src/main/java/ru/hogwards/shcool/HogwardsSchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwards.shcool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwardsSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwardsSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwards/shcool/HogwardsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwards.shcool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwardsSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
